35785581141ca995e1b956c1db0976d62bb84a1b
Oumar Mimouni <oumar.mimouni@gmail.com>
2025-04-03 10:35:57 +0000

    I made a simple experimental plugin for tailwind to dynamically reset styles on uprent’s component in the extension

--- apps/extension/tailwind.config.js
+++ apps/extension/tailwind.config.js
@@ -1,3 +1,7 @@
+const postcss = require('postcss');
+const plugin = require('tailwindcss/plugin');
+const fs = require('fs');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   // eslint-disable-next-line no-undef
@@ -11,7 +15,37 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // Experimental: Tailwind CSS plugin to add base styles to the app
+    plugin(function({ addBase }) {
+      try {
+
+        // read the preflight.css file
+        const baseStyles = postcss.parse(
+          fs.readFileSync('../../node_modules/tailwindcss/lib/css/preflight.css', 'utf8')
+        );
+
+        // go through each rule in the base styles
+        baseStyles.walkRules((rule) => {
+          // map over the selectors
+          rule.selectors = rule.selectors.map((selector) => {
+            // If the selector is html, body, or :host, add the .uprent-reset class
+            // Otherwise, add the .uprent-reset class to the selector
+            return ['html', 'body', ':host'].includes(selector)
+              ? '.uprent-reset'
+              : ':where(.uprent-reset) ' + selector;
+          });
+        });
+
+        // add the base styles to the app
+        addBase(baseStyles.nodes);
+        console.log("Successfully added base styles");
+      } catch (error) {
+        console.error("Error in Tailwind plugin:", error);
+      }
+    })
+  ],
+
   corePlugins: {
     preflight: false,
   },
